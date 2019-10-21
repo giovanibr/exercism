6cf874f99c994a99bca213d735ea8081d887d772
08561225726 <giovani.racca@serpro.gov.br>
2019-10-21 18:48:37 -0300

    update solutions for kotlin and js tracks

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -3,7 +3,7 @@ object Hamming {
     fun compute(strand1: String = "", strand2: String = ""): Int {
         require(strand1.length == strand2.length) { "left and right strands must be of equal length." }
 
-        val pair: List<Pair<Char, Char>> = strand1.zip(strand2)
+        val pair = strand1.zip(strand2)
 
         return pair.count{it.first != it.second}
 
